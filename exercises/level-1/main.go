0b3bdaf8029bc5b8318822009cfcdebacc619948
Ritesh Harjani <ritesh.harjani@gmail.com>
2020-10-26 11:35:43 +0530

    level-1: question-5
    Signed-off-by: Ritesh Harjani <ritesh.harjani@gmail.com>

--- exercises/level-1/main.go
+++ exercises/level-1/main.go
@@ -7,9 +7,28 @@ func question_4() {
 	// https://golang.org/ref/spec#Types
 	type mytype string
 	var v mytype = "hello world"
+	fmt.Printf("==============================\n")
 	fmt.Printf("type = %T\nvalue = %v\n", v, v)
+	fmt.Printf("==============================\n\n")
 }
 
+func question_5() {
+	// underlying type of mytype is string
+	// https://golang.org/ref/spec#Types
+	type mytype string
+	var v mytype = "hello world"
+
+	// assigning value from mytype "v" to string type "x"
+	// This is called as conversion and not casting
+	var x string
+	x = string(v)
+
+	fmt.Printf("==============================\n")
+	fmt.Printf("type = %T\nvalue = %v\n", v, v)
+	fmt.Println(x)
+	fmt.Printf("==============================\n\n")
+}
 func main() {
 	question_4()
+	question_5()
 }
